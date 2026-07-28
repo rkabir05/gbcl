6c4c655e88fcbac2af89404b0994d715e5470805
rkabir05 <rkabir05@gmail.com>
2026-07-28 13:48:03 +0600

    header and footer update

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,35 @@
           prevEl: '.swiper-button-prev',
         },
       });
+
+
+                  $('.bxslider-business').bxSlider({
+                mode: 'horizontal',
+                minSlides: 4,
+                maxSlides: 4,
+                moveSlides: 6,
+                slideWidth: 450,
+                slideMargin: 20,
+                pager: false,
+                controls: false,
+                infiniteLoop: true
+            });
+
+            $('.bxslider-logo').bxSlider({
+
+                minSlides: window.innerWidth < 576 ? 1 :
+                        window.innerWidth < 768 ? 2 :
+                        window.innerWidth < 992 ? 3 : 5,
+
+                maxSlides: window.innerWidth < 576 ? 1 :
+                        window.innerWidth < 768 ? 2 :
+                        window.innerWidth < 992 ? 3 : 5,
+
+                minSlides: 4,
+                maxSlides: 4,            
+                slideWidth:170,
+                slideMargin:10,
+                ticker:true,
+                speed:30000
+
+            });
