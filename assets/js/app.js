c09a21ff30bc87f3d0bfefa4945f3c2ae69b496b
rkabir05 <rkabir05@gmail.com>
2026-07-28 13:08:28 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+      var swiper = new Swiper('.mySwiper', {
+        slidesPerView: 5,
+        // centeredSlides: true,
+        spaceBetween: 20,
+        grabCursor: true,
+        // pagination: {
+        //   el: '.swiper-pagination',
+        //   clickable: true,
+        // },
+
+                navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
